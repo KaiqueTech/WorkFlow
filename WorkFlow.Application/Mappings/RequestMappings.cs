46cccb444736d9d728ddf7642cf0cb6d43af8919
KaiqueTech <developerkaique99@gmail.com>
2026-02-17 21:43:06 -0300

    feat(application): padroniza criação de Commands e melhora legibilidade
    Adiciona mapping para criação do CreateRequestCommand
    
    Aplica mapping também para o ApproveRequestCommand
    
    Utiliza argumentos nomeados para tornar os parâmetros explícitos e reduzir erros por ordem incorreta
    
    Melhora organização e padronização seguindo boas práticas de CQRS

---
+++ WorkFlow.Application/Mappings/RequestMappings.cs
@@ -0,0 +1,25 @@
+﻿using WorkFlow.Application.Commands.ApproveRequest;
+using WorkFlow.Application.Commands.CreateRequest;
+using WorkFlow.Application.Commands.RejectRequest;
+using WorkFlow.Application.DTOs;
+
+namespace WorkFlow.Application.Mappings
+{
+    public static class RequestMappings
+    {
+        public static CreateRequestCommand ToCommand(this CreateRequestDto dto, string userId) 
+        {
+            return new CreateRequestCommand(dto.Title, dto.Description, dto.Category, dto.Priority, userId);
+        }
+
+        public static ApproveRequestCommand ToApproveCommand(this ApprovalDto dto, Guid requestId,string managerId,string? userRole)
+        {
+            return new ApproveRequestCommand(requestId,managerId,userRole ?? "User",dto.comment);
+        }
+
+        public static RejectRequestCommand ToRejectCommand(this RejectionDto dto, Guid requestId, string managerId, string? userRole) 
+        {
+            return new RejectRequestCommand(requestId, managerId, userRole ?? "User", dto.comment);
+        }
+    }
+}
